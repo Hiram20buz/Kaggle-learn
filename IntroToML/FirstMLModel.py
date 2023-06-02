138c4c1be47e82fceb1fe707a809f0449717ff44
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-06-01 20:16:54 -0700

    Update FirstMLModel.py

--- IntroToML/FirstMLModel.py
+++ IntroToML/FirstMLModel.py
@@ -1,15 +1,5 @@
 import pandas as pd
 
 melbourne_file_path = 'train.csv'
-melbourne_data = pd.read_csv(melbourne_file_path) 
-melbourne_data.columns
-# dropna drops missing values (think of na as "not available")
-melbourne_data = melbourne_data.dropna(axis=0)
-
-y = melbourne_data.Price
-
-melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
-
-X = melbourne_data[melbourne_features]
-X.describe()
-X.head()
+melbourne_data = pd.read_csv(melbourne_file_path)
+print(melbourne_data.columns)
