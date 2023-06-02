f83ea4f0f5c90700f487ce2fca54ef8652ca106c
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-06-01 20:10:02 -0700

    Create BasicDataExploration.py

---
+++ IntroToML/BasicDataExploration.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# Define the URL
+melbourne_url = 'train.csv'
+
+# Read the data from the URL and store it in a DataFrame titled melbourne_data
+melbourne_data = pd.read_csv(melbourne_url)
+
+# Print a summary of the data in Melbourne data
+print(melbourne_data.describe())
