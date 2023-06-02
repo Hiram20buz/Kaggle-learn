f498c60d06cdd02a1c429e38d6e418571a570c54
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-06-01 20:00:47 -0700

    Create FirstMLModel.py

---
+++ FirstMLModel.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+melbourne_file_path = 'https://raw.githubusercontent.com/scpike/us-state-county-zip/master/geo-data.csv'
+melbourne_data = pd.read_csv(melbourne_file_path) 
+melbourne_data.columns
+# dropna drops missing values (think of na as "not available")
+melbourne_data = melbourne_data.dropna(axis=0)
+
+y = melbourne_data.Price
+
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+
+X = melbourne_data[melbourne_features]
+X.describe()
+X.head()
